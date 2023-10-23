a03d1da6536b30da071e402f1d583686ad3b1564
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-23 16:32:33 +0530

    p1: code interview questions 2, 3

--- part1/interview-questions/solutions/src/q2_union_find_with_specific_canonical_element/UnionFindCanonical.java
+++ part1/interview-questions/solutions/src/q2_union_find_with_specific_canonical_element/UnionFindCanonical.java
@@ -1,5 +1,81 @@
 package q2_union_find_with_specific_canonical_element;
 
 public class UnionFindCanonical {
+    private int numSets;
+    private int[] id;
+    private int[] size;
+    private int[] biggestNumber;
+
+    public UnionFindCanonical(int n) {
+        this.numSets = n;
+        this.id = new int[n];
+        this.size = new int[n];
+        this.biggestNumber = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            biggestNumber[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    // Uses path compression
+    private int getRoot(int i) {
+        if (i != id[i]) {
+            id[i] = getRoot(id[i]);
+        }
+        return id[i];
+    }
+
+    public int find(int i) {
+        int root = getRoot(i);
+        return biggestNumber[root];
+    }
+
+    // Weighted union
+    public void union(int i, int j) {
+        int iRoot = getRoot(i);
+        int jRoot = getRoot(j);
+
+        if (iRoot == jRoot)
+            return;
+
+        numSets--;
+        int biggestNumberInBothTrees = Integer.max(biggestNumber[iRoot], biggestNumber[jRoot]);
+        if (size[iRoot] < size[jRoot]) {
+            id[iRoot] = jRoot;
+            size[jRoot] += size[iRoot];
+            biggestNumber[jRoot] = biggestNumberInBothTrees;
+        } else {
+            id[jRoot] = iRoot;
+            size[iRoot] += size[jRoot];
+            biggestNumber[iRoot] = biggestNumberInBothTrees;
+        }
+    }
+
+    public int count() {
+        return numSets;
+    }
+
+    public static void main(String[] args) {
+        UnionFindCanonical ufc = new UnionFindCanonical(5);
+
+        System.out.println(ufc.count());
+
+        ufc.union(0, 1);
+
+        System.out.println(ufc.find(0));
+        System.out.println(ufc.find(1));
+
+        ufc.union(0, 2);
+        ufc.union(0, 4);
+
+        System.out.println(ufc.find(0));
+        System.out.println(ufc.find(1));
+        System.out.println(ufc.find(2));
+        System.out.println(ufc.find(4));
+
+        System.out.println(ufc.count());
+    }
 
 }
--- part1/interview-questions/solutions/src/q3_successor_with_delete/SuccessorWithDelete.java
+++ part1/interview-questions/solutions/src/q3_successor_with_delete/SuccessorWithDelete.java
@@ -1,5 +1,55 @@
 package q3_successor_with_delete;
 
+import q2_union_find_with_specific_canonical_element.UnionFindCanonical;
+
 public class SuccessorWithDelete {
-    
+    private UnionFindCanonical ufc;
+    private boolean[] isRemoved;
+
+    public SuccessorWithDelete(int n) {
+        ufc = new UnionFindCanonical(n);
+        isRemoved = new boolean[n];
+
+        // Mark all elements as not removed initially
+        for (int i = 0; i < isRemoved.length; i++) {
+            isRemoved[i] = false;
+        }
+    }
+
+    /**
+     * Variant: we have sets of deleted elements
+     * We maintain those sets using union find canonical
+     * for which each set has its root as the biggest value in that set
+     * 
+     * @param x
+     * @return the value of the next unremoved element
+     * @return n, if no such element exists
+     */
+    public int getSuccessorAfterDelete(int x) {
+        // If not already removed
+        // we mark as removed
+        // and union it with left and right elements if they are
+        // already removed
+        if (!isRemoved[x]) {
+            isRemoved[x] = true;
+            if (x - 1 >= 0 && isRemoved[x - 1])
+                ufc.union(x - 1, x);
+            if (x + 1 < isRemoved.length && isRemoved[x + 1])
+                ufc.union(x, x + 1);
+        }
+
+        // We get the largest element in that
+        // set using the uf data structure and return the number after that
+        return ufc.find(x) + 1;
+    }
+
+    public static void main(String[] args) {
+        SuccessorWithDelete s = new SuccessorWithDelete(5);
+        System.out.println(s.getSuccessorAfterDelete(0));
+        System.out.println(s.getSuccessorAfterDelete(2));
+        System.out.println(s.getSuccessorAfterDelete(4));
+        System.out.println(s.getSuccessorAfterDelete(1));
+        System.out.println(s.getSuccessorAfterDelete(3));
+        System.out.println(s.getSuccessorAfterDelete(0));
+    }
 }

7e88df594fd5cafc787da1993f0ad20b458e29e1
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-26 09:25:37 +0530

    p1: add array equals method to test utils

--- part1/interview-questions/solutions/src/test_utils/TestUtils.java
+++ part1/interview-questions/solutions/src/test_utils/TestUtils.java
@@ -10,4 +10,24 @@ public class TestUtils {
         if (!expression)
             throw new AssertionError(message);
     }
+
+    public static <T> boolean arrayEquals(T[] t1, T[] t2) {
+        // If array lengths are different, false
+        if (t1.length != t2.length) {
+            return false;
+        }
+
+        // If both arrays are same object, true
+        if (t1 == t2)
+            return true;
+
+        // If any of the elements aren't equal, false
+        for (int i = 0; i < t1.length; i++) {
+            if (!t1[i].equals(t2[i]))
+                return false;
+        }
+
+        // True if we reach here
+        return true;
+    }
 }

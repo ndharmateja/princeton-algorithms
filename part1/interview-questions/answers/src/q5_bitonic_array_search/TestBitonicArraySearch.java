10fbe9e00281db638c52f1541fcdc7f516e9a4ce
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-10-26 15:39:21 +0530

    p1: interview question 5 fix bug

--- part1/interview-questions/answers/src/q5_bitonic_array_search/TestBitonicArraySearch.java
+++ part1/interview-questions/answers/src/q5_bitonic_array_search/TestBitonicArraySearch.java
@@ -5,64 +5,82 @@ import java.util.Arrays;
 import test_utils.TestUtils;
 
 public class TestBitonicArraySearch {
-    public static void main(String[] args) {
-        int[][] arrays = new int[][] {
-                { 1, 2, -3, -5 },
-                { 1, 2, -3, -5 },
-                { 1, 2, -3, -5 },
-                { 1, 2, -3, -5 },
-                { 1, 2, -3, -5 },
-                { 1, 2, -3, -5 },
-                { 1, 2, -3 },
-                { 1, 2, -3 },
-                { 1, 2, -3 },
-                { 1, 2, -3 },
-                { -5, -4, -3, 1, 2, 5, -15 },
-                { -5, -4, -3, 1, 2, 5, -15 },
-                { -5, -4, -3, 1, 2, 5, -15 },
-                { -5, -4, -3, 1, 2, 5, -15 },
-        };
-        int[] targets = new int[] {
-                1,
-                2,
-                -3,
-                -5,
-                -10,
-                0,
-                1,
-                2,
-                -3,
-                -5,
-                2,
-                -15,
-                -5,
-                -7
-        };
-        int[] expectedOutputs = new int[] {
-                0,
-                1,
-                2,
-                3,
-                -1,
-                -1,
-                0,
-                1,
-                2,
-                -1,
-                4,
-                6,
-                0,
-                -1
-        };
+        public static void main(String[] args) {
+                int[][] arrays = new int[][] {
+                                { 1, 2, -3, -5 },
+                                { 1, 2, -3, -5 },
+                                { 1, 2, -3, -5 },
+                                { 1, 2, -3, -5 },
+                                { 1, 2, -3, -5 },
+                                { 1, 2, -3, -5 },
+                                { 1, 2, -3 },
+                                { 1, 2, -3 },
+                                { 1, 2, -3 },
+                                { 1, 2, -3 },
+                                { -5, -4, -3, 1, 2, 5, -15 },
+                                { -5, -4, -3, 1, 2, 5, -15 },
+                                { -5, -4, -3, 1, 2, 5, -15 },
+                                { -5, -4, -3, 1, 2, 5, -15 },
+                                { 3, 5, 3, 2, 0 },
+                                { 3, 5, 3, 2, 0 },
+                };
+                int[] targets = new int[] {
+                                1,
+                                2,
+                                -3,
+                                -5,
+                                -10,
+                                0,
 
-        for (int i = 0; i < arrays.length; i++) {
-            int[] array = arrays[i];
-            int target = targets[i];
-            int expectedOutput = expectedOutputs[i];
-            int actualOutput = BitonicArray.search(array, target);
-            TestUtils.doAssertion(expectedOutput == actualOutput, "Array: " + Arrays.toString(array) + ", target: "
-                    + target + ", expected: " + expectedOutput + ", actual: " + actualOutput);
+                                1,
+                                2,
+                                -3,
+                                -5,
+
+                                2,
+                                -15,
+                                -5,
+                                -7,
+
+                                3,
+                                5,
+                                2,
+                                0
+                };
+                int[] expectedOutputs = new int[] {
+                                0,
+                                1,
+                                2,
+                                3,
+                                -1,
+                                -1,
+
+                                0,
+                                1,
+                                2,
+                                -1,
+
+                                4,
+                                6,
+                                0,
+                                -1,
+
+                                0,
+                                1,
+                                3,
+                                4
+                };
+
+                for (int i = 0; i < arrays.length; i++) {
+                        int[] array = arrays[i];
+                        int target = targets[i];
+                        int expectedOutput = expectedOutputs[i];
+                        int actualOutput = BitonicArray.search(array, target);
+                        TestUtils.doAssertion(expectedOutput == actualOutput,
+                                        "Array: " + Arrays.toString(array) + ", target: "
+                                                        + target + ", expected: " + expectedOutput + ", actual: "
+                                                        + actualOutput);
+                }
+                System.out.println("Tests passed");
         }
-        System.out.println("Tests passed");
-    }
 }
--- part1/interview-questions/answers/src/q5_bitonic_array_search/binary_search/BinarySearchMaximumIndex.java
+++ part1/interview-questions/answers/src/q5_bitonic_array_search/binary_search/BinarySearchMaximumIndex.java
@@ -4,6 +4,14 @@ public class BinarySearchMaximumIndex extends BinarySearch {
 
     @Override
     protected boolean shouldGoLeft(int[] array, int mid, int target) {
+        // If we are at start of array
+        // we have to go right
+        // and vice versa
+        if (mid == 0)
+            return false;
+        if (mid == array.length - 1)
+            return true;
+
         // While looking for maximum index in bitonic array
         // we go left when array[mid - 1], array[mid], array[mid + 1]
         // are in decreasing order (which means mid is in the decreasing
@@ -13,6 +21,14 @@ public class BinarySearchMaximumIndex extends BinarySearch {
 
     @Override
     protected boolean shouldGoRight(int[] array, int mid, int target) {
+        // If we are at start of array
+        // we have to go right
+        // and vice versa
+        if (mid == 0)
+            return true;
+        if (mid == array.length - 1)
+            return false;
+
         // While looking for maximum index in bitonic array
         // we go right when array[mid - 1], array[mid], array[mid + 1]
         // are in incrasing order (which means mid is in the increasing

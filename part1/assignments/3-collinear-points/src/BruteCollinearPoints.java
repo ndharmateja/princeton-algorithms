cfbaefdcb8a8290455ec4f95062a7819e8a14585
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-11-01 19:18:24 +0530

    p1a3: hardcode filename

--- part1/assignments/3-collinear-points/src/BruteCollinearPoints.java
+++ part1/assignments/3-collinear-points/src/BruteCollinearPoints.java
@@ -85,7 +85,9 @@ public class BruteCollinearPoints {
 
     public static void main(String[] args) {
         // read the n points from a file
-        In in = new In(args[0]);
+        // String filename = args[0];
+        String filename = "data/equidistant.txt";
+        In in = new In(filename);
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {

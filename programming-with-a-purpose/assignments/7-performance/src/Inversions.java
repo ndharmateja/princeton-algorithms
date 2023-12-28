ecc0958da5b5182f14c7c3b3d320b5e22e4fbfbd
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-12-28 18:08:42 +0530

    pca7: init

---
+++ programming-with-a-purpose/assignments/7-performance/src/Inversions.java
@@ -0,0 +1,17 @@
+public class Inversions {
+
+    // Return the number of inversions in the permutation a[].
+    public static long count(int[] a) {
+        return -1l;
+    }
+
+    // Return a permutation of length n with exactly k inversions.
+    public static int[] generate(int n, long k) {
+        return null;
+    }
+
+    // Takes an integer n and a long k as command-line arguments,
+    // and prints a permutation of length n with exactly k inversions.
+    public static void main(String[] args) {
+    }
+}

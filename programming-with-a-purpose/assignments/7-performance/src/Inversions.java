8b02fe6c13861e440eaa08b331a24546fb58e7bc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-12-28 20:01:44 +0530

    pca7: solve parts 1 and 2

--- programming-with-a-purpose/assignments/7-performance/src/Inversions.java
+++ programming-with-a-purpose/assignments/7-performance/src/Inversions.java
@@ -1,17 +1,103 @@
 public class Inversions {
-
     // Return the number of inversions in the permutation a[].
     public static long count(int[] a) {
-        return -1l;
+        long count = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j])
+                    count++;
+            }
+        }
+        return count;
     }
 
-    // Return a permutation of length n with exactly k inversions.
+    /**
+     * Return a permutation of length n with exactly k inversions.
+     * 
+     * Method:
+     * 1. We will try to put the number 'n-1' at 0 (to create n-1 inversions)
+     * and then try to put the number 'n-2' at 1 (to create n-2 inversions)
+     * 2. We keep doing that until we reach a point where number of inversions
+     * will become zero and we can insert 0, 1, 2, 3.. in order after the last
+     * inserted number (which was created for inversions)
+     * 3. Another case is num_inversions < n-k, then we have to insert
+     * n-k in middle of remaining array
+     * 
+     * Eg: n = 10, k = 20
+     * array: [ _, _, _, _, _, _, _, _, _, _ ]
+     * k: 20
+     * 
+     * After putting n-1=9
+     * array: [ 9, _, _, _, _, _, _, _, _, _]
+     * k: 11
+     * 
+     * After putting n-2=8
+     * array: [ 9, 8, _, _, _, _, _, _, _, _ ]
+     * k: 3
+     * 
+     * Now k < n-3
+     * so we insert 7 in middle so that we still get k extra inversions
+     * array: [ 9, 8, _, _, _, _, 7, _, _, _ ]
+     * k: 0
+     * 
+     * Now fill the remaining slots with 0, 1, 2, 3..
+     * array: [ 9, 8, 0, 1, 2, 3, 7, 4, 5, 6 ]
+     * 
+     * @param n
+     * @param k
+     * @return
+     */
     public static int[] generate(int n, long k) {
-        return null;
+        // Base case
+        int[] array = new int[n];
+        if (n == 0)
+            return array;
+
+        int r = n - 1;
+        int l = 0;
+        while (true) {
+            // k != 0 && k >= r
+            if (k != 0 && k >= r) {
+                k -= r;
+                array[l++] = r--;
+            }
+            // The above 'if' condition will eventually be
+            // reduced to one of the below cases
+            // k == 0 || k < r
+            else {
+                // Index to insert 'r'
+                // to add the remaining 'k' extra inversions
+                int index = (n - 1) - (int) k;
+                array[index] = r;
+
+                // Fill remaining slots with 0, 1, 2, 3..
+                int curr = 0;
+                while (l < array.length) {
+                    if (l == index) {
+                        l++;
+                        continue;
+                    }
+                    array[l++] = curr++;
+                }
+                break;
+            }
+        }
+
+        return array;
+    }
+
+    private static void printArray(int[] array) {
+        for (int x : array) {
+            StdOut.print(x + " ");
+        }
+        StdOut.println();
     }
 
     // Takes an integer n and a long k as command-line arguments,
     // and prints a permutation of length n with exactly k inversions.
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        long k = Long.parseLong(args[1]);
+        printArray(generate(n, k));
     }
 }
--- programming-with-a-purpose/assignments/7-performance/src/Ramanujan.java
+++ programming-with-a-purpose/assignments/7-performance/src/Ramanujan.java
@@ -1,12 +1,51 @@
 public class Ramanujan {
+    private static long cube(long i) {
+        return i * i * i;
+    }
 
     // Is n a Ramanujan number?
     public static boolean isRamanujan(long n) {
+        int cubeRoot = (int) Math.floor(Math.cbrt(n));
+
+        long a = -1, b = -1;
+        for (long i = 1; i <= cubeRoot; i++) {
+            // If we are looking at an i which is already a or b
+            // then that pair is already found and we don't need to
+            // look at this i
+            // (initial values of -1 works when looking at every i for the first time)
+            if (i == a || i == b)
+                continue;
+
+            // Find the compliment number of i = (n - i^3)^(1/3)
+            int iCompliment = (int) Math.round(Math.cbrt(n - i * i * i));
+
+            // If i^3 + iCompliment^3 != n
+            // we can skip this i
+            if (cube(i) + cube(iCompliment) != n)
+                continue;
+
+            // If there is a pair which is already found
+            // which means this is the second distinct pair
+            // we can return true
+            if (a != -1)
+                return true;
+
+            // If finding for the first time
+            // update the values of a and b
+            a = i;
+            b = iCompliment;
+        }
+
+        // If we reach here => two distinct pairs don't exist
+        // and we return false
         return false;
     }
 
     // Takes a long integer command-line arguments n and prints true if
     // n is a Ramanujan number, and false otherwise.
     public static void main(String[] args) {
+        long n = Long.parseLong(args[0]);
+        // long n = 1729;
+        StdOut.println(isRamanujan(n));
     }
 }

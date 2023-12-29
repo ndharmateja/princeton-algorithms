ad8e96941e8f241d8a5236df52692d2db91e93f3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-12-30 00:31:32 +0530

    pca8: init

---
+++ programming-with-a-purpose/assignments/8-oop1/src/Huntingtons.java
@@ -0,0 +1,27 @@
+public class Huntingtons {
+
+    // Returns the maximum number of consecutive repeats of CAG in the DNA string.
+    public static int maxRepeats(String dna) {
+        return -1;
+    }
+
+    // Returns a copy of s, with all whitespace (spaces, tabs, and newlines)
+    // removed.
+    public static String removeWhitespace(String s) {
+        return null;
+    }
+
+    // Returns one of these diagnoses corresponding to the maximum number of
+    // repeats:
+    // "not human", "normal", "high risk", or "Huntington's".
+    public static String diagnose(int maxRepeats) {
+        return null;
+    }
+
+    // Sample client (see below).
+    public static void main(String[] args) {
+        String filename = args[0];
+        StdOut.println(filename);
+    }
+
+}

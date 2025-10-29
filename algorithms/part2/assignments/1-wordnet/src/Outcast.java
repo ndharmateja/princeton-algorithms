cccbc64f6c64246798bea4b1c26d7cd1d7cec3d3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-10-29 17:26:52 +0530

    remove 2d array implementation

--- algorithms/part2/assignments/1-wordnet/src/Outcast.java
+++ algorithms/part2/assignments/1-wordnet/src/Outcast.java
@@ -14,41 +14,24 @@ public class Outcast {
         // We store the distances in the 2d array so that
         // we don't do duplicate computations
         int n = nouns.length;
-        int[][] pairwiseDistances = new int[n][n];
 
-        // This array stores the total outcast distance for each nount
-        int[] outcastDistances = new int[n];
+        // Base case
+        if (n == 1 || n == 2)
+            return nouns[0];
 
-        // We go row by row from left to right
-        // For a row when r < c, we are on the lower triangular part
-        // so [r][c] has already been computed in the [c][r] as it is a symmetric array
-        // so we take the value from the symmetric counter part when r < c
-        //
-        // For each row as we go from left to right we also compute
-        // the total distance and store the result in the outcastDistances array
-        for (int r = 0; r < n; r++) {
-            int rthDistance = 0;
-            for (int c = 0; c < n; c++) {
-                if (r == c)
-                    pairwiseDistances[r][c] = 0;
-                else if (r > c)
-                    pairwiseDistances[r][c] = pairwiseDistances[c][r];
-                else
-                    pairwiseDistances[r][c] = this.wordNet.distance(nouns[r], nouns[c]);
-
-                rthDistance += pairwiseDistances[r][c];
-            }
-
-            outcastDistances[r] = rthDistance;
-        }
-
+        // For each vertex, loop over all the remaining vertices
+        // and find the sum of the pairwise distances
         // Find the index with the max outcast distance
-        int outcast = 0;
-        int maxOutcastDistance = outcastDistances[0];
-        for (int i = 1; i < n; i++) {
-            if (outcastDistances[i] > maxOutcastDistance) {
-                maxOutcastDistance = outcastDistances[i];
-                outcast = i;
+        int outcast = -1;
+        int maxOutcastDistance = 0;
+        for (int v = 0; v < n; v++) {
+            int distance = 0;
+            for (int i = 0; i < n; i++) {
+                distance += this.wordNet.distance(nouns[v], nouns[i]);
+            }
+            if (distance > maxOutcastDistance) {
+                outcast = v;
+                maxOutcastDistance = distance;
             }
         }
 
